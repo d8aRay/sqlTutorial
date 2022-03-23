9bb8c9dccfced04a9bf0059d6b80d1cdc7d941b7
ray <redwards1230@gmail.com>
2022-03-23 17:18:06 -0400

    fixed `sql_tutorial.countries` to run on one insert

--- createDB.sql
+++ createDB.sql
@@ -16,31 +16,33 @@ CREATE TABLE `sql_tutorial.countries` (
 	region_id INTEGER NOT NULL,
 );
 
-INSERT INTO `sql_tutorial.countries`(country_id,country_name,region_id) VALUES ('AR','Argentina',2);
-INSERT INTO `sql_tutorial.countries`(country_id,country_name,region_id) VALUES ('AU','Australia',3);
-INSERT INTO `sql_tutorial.countries`(country_id,country_name,region_id) VALUES ('BE','Belgium',1);
-INSERT INTO `sql_tutorial.countries`(country_id,country_name,region_id) VALUES ('BR','Brazil',2);
-INSERT INTO `sql_tutorial.countries`(country_id,country_name,region_id) VALUES ('CA','Canada',2);
-INSERT INTO `sql_tutorial.countries`(country_id,country_name,region_id) VALUES ('CH','Switzerland',1);
-INSERT INTO `sql_tutorial.countries`(country_id,country_name,region_id) VALUES ('CN','China',3);
-INSERT INTO `sql_tutorial.countries`(country_id,country_name,region_id) VALUES ('DE','Germany',1);
-INSERT INTO `sql_tutorial.countries`(country_id,country_name,region_id) VALUES ('DK','Denmark',1);
-INSERT INTO `sql_tutorial.countries`(country_id,country_name,region_id) VALUES ('EG','Egypt',4);
-INSERT INTO `sql_tutorial.countries`(country_id,country_name,region_id) VALUES ('FR','France',1);
-INSERT INTO `sql_tutorial.countries`(country_id,country_name,region_id) VALUES ('HK','HongKong',3);
-INSERT INTO `sql_tutorial.countries`(country_id,country_name,region_id) VALUES ('IL','Israel',4);
-INSERT INTO `sql_tutorial.countries`(country_id,country_name,region_id) VALUES ('IN','India',3);
-INSERT INTO `sql_tutorial.countries`(country_id,country_name,region_id) VALUES ('IT','Italy',1);
-INSERT INTO `sql_tutorial.countries`(country_id,country_name,region_id) VALUES ('JP','Japan',3);
-INSERT INTO `sql_tutorial.countries`(country_id,country_name,region_id) VALUES ('KW','Kuwait',4);
-INSERT INTO `sql_tutorial.countries`(country_id,country_name,region_id) VALUES ('MX','Mexico',2);
-INSERT INTO `sql_tutorial.countries`(country_id,country_name,region_id) VALUES ('NG','Nigeria',4);
-INSERT INTO `sql_tutorial.countries`(country_id,country_name,region_id) VALUES ('NL','Netherlands',1);
-INSERT INTO `sql_tutorial.countries`(country_id,country_name,region_id) VALUES ('SG','Singapore',3);
-INSERT INTO `sql_tutorial.countries`(country_id,country_name,region_id) VALUES ('UK','United Kingdom',1);
-INSERT INTO `sql_tutorial.countries`(country_id,country_name,region_id) VALUES ('US','United States of America',2);
-INSERT INTO `sql_tutorial.countries`(country_id,country_name,region_id) VALUES ('ZM','Zambia',4);
-INSERT INTO `sql_tutorial.countries`(country_id,country_name,region_id) VALUES ('ZW','Zimbabwe',4);
+INSERT INTO `sql_tutorial.countries`(country_id,country_name,region_id) VALUES 
+  ('AR','Argentina',2)
+, ('AU','Australia',3)
+, ('BE','Belgium',1)
+, ('BR','Brazil',2)
+, ('CA','Canada',2)
+, ('CH','Switzerland',1)
+, ('CN','China',3)
+, ('DE','Germany',1)
+, ('DK','Denmark',1)
+, ('EG','Egypt',4)
+, ('FR','France',1)
+, ('HK','HongKong',3)
+, ('IL','Israel',4)
+, ('IN','India',3)
+, ('IT','Italy',1)
+, ('JP','Japan',3)
+, ('KW','Kuwait',4)
+, ('MX','Mexico',2)
+, ('NG','Nigeria',4)
+, ('NL','Netherlands',1)
+, ('SG','Singapore',3)
+, ('UK','United Kingdom',1)
+, ('US','United States of America',2)
+, ('ZM','Zambia',4)
+, ('ZW','Zimbabwe',4)
+;
 
 CREATE TABLE `sql_tutorial.locations` (
 	location_id INTEGER,
