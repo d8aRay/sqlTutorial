6388f2c72a331e988e9bb0ed331c1d50c524867f
ray <redwards1230@gmail.com>
2022-03-23 17:37:31 -0400

    inserting employees on one insert

--- createDB.sql
+++ createDB.sql
@@ -123,47 +123,48 @@ CREATE TABLE `sql_tutorial.employees` (
 	department_id INTEGER,
 );
 
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (100,'Steven','King','steven.king@sqltutorial.org','515.123.4567','1987-06-17',4,240000.00,NULL,9);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (101,'Neena','Kochhar','neena.kochhar@sqltutorial.org','515.123.4568','1989-09-21',5,170000.00,100,9);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (102,'Lex','De Haan','lex.de haan@sqltutorial.org','515.123.4569','1993-01-13',5,170000.00,100,9);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (103,'Alexander','Hunold','alexander.hunold@sqltutorial.org','590.423.4567','1990-01-03',9,90000.00,102,6);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (104,'Bruce','Ernst','bruce.ernst@sqltutorial.org','590.423.4568','1991-05-21',9,60000.00,103,6);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (105,'David','Austin','david.austin@sqltutorial.org','590.423.4569','1997-06-25',9,48000.00,103,6);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (106,'Valli','Pataballa','valli.pataballa@sqltutorial.org','590.423.4560','1998-02-05',9,48000.00,103,6);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (107,'Diana','Lorentz','diana.lorentz@sqltutorial.org','590.423.5567','1999-02-07',9,42000.00,103,6);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (108,'Nancy','Greenberg','nancy.greenberg@sqltutorial.org','515.124.4569','1994-08-17',7,120000.00,101,10);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (109,'Daniel','Faviet','daniel.faviet@sqltutorial.org','515.124.4169','1994-08-16',6,90000.00,108,10);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (110,'John','Chen','john.chen@sqltutorial.org','515.124.4269','1997-09-28',6,82000.00,108,10);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (111,'Ismael','Sciarra','ismael.sciarra@sqltutorial.org','515.124.4369','1997-09-30',6,77000.00,108,10);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (112,'Jose Manuel','Urman','jose manuel.urman@sqltutorial.org','515.124.4469','1998-03-07',6,78000.00,108,10);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (113,'Luis','Popp','luis.popp@sqltutorial.org','515.124.4567','1999-12-07',6,69000.00,108,10);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (114,'Den','Raphaely','den.raphaely@sqltutorial.org','515.127.4561','1994-12-07',14,110000.00,100,3);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (115,'Alexander','Khoo','alexander.khoo@sqltutorial.org','515.127.4562','1995-05-18',13,31000.00,114,3);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (116,'Shelli','Baida','shelli.baida@sqltutorial.org','515.127.4563','1997-12-24',13,29000.00,114,3);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (117,'Sigal','Tobias','sigal.tobias@sqltutorial.org','515.127.4564','1997-07-24',13,28000.00,114,3);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (118,'Guy','Himuro','guy.himuro@sqltutorial.org','515.127.4565','1998-11-15',13,26000.00,114,3);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (119,'Karen','Colmenares','karen.colmenares@sqltutorial.org','515.127.4566','1999-08-10',13,25000.00,114,3);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (120,'Matthew','Weiss','matthew.weiss@sqltutorial.org','650.123.1234','1996-07-18',19,80000.00,100,5);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (121,'Adam','Fripp','adam.fripp@sqltutorial.org','650.123.2234','1997-04-10',19,82000.00,100,5);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (122,'Payam','Kaufling','payam.kaufling@sqltutorial.org','650.123.3234','1995-05-01',19,79000.00,100,5);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (123,'Shanta','Vollman','shanta.vollman@sqltutorial.org','650.123.4234','1997-10-10',19,65000.00,100,5);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (126,'Irene','Mikkilineni','irene.mikkilineni@sqltutorial.org','650.124.1224','1998-09-28',18,27000.00,120,5);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (145,'John','Russell','john.russell@sqltutorial.org',NULL,'1996-10-01',15,140000.00,100,8);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (146,'Karen','Partners','karen.partners@sqltutorial.org',NULL,'1997-01-05',15,135000.00,100,8);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (176,'Jonathon','Taylor','jonathon.taylor@sqltutorial.org',NULL,'1998-03-24',16,86000.00,100,8);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (177,'Jack','Livingston','jack.livingston@sqltutorial.org',NULL,'1998-04-23',16,84000.00,100,8);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (178,'Kimberely','Grant','kimberely.grant@sqltutorial.org',NULL,'1999-05-24',16,70000.00,100,8);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (179,'Charles','Johnson','charles.johnson@sqltutorial.org',NULL,'2000-01-04',16,62000.00,100,8);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (192,'Sarah','Bell','sarah.bell@sqltutorial.org','650.501.1876','1996-02-04',17,40000.00,123,5);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (193,'Britney','Everett','britney.everett@sqltutorial.org','650.501.2876','1997-03-03',17,39000.00,123,5);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (200,'Jennifer','Whalen','jennifer.whalen@sqltutorial.org','515.123.4444','1987-09-17',3,44000.00,101,1);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (201,'Michael','Hartstein','michael.hartstein@sqltutorial.org','515.123.5555','1996-02-17',10,130000.00,100,2);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (202,'Pat','Fay','pat.fay@sqltutorial.org','603.123.6666','1997-08-17',11,60000.00,201,2);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (203,'Susan','Mavris','susan.mavris@sqltutorial.org','515.123.7777','1994-06-07',8,65000.00,101,4);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (204,'Hermann','Baer','hermann.baer@sqltutorial.org','515.123.8888','1994-06-07',12,100000.00,101,7);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (205,'Shelley','Higgins','shelley.higgins@sqltutorial.org','515.123.8080','1994-06-07',2,120000.00,101,11);
-INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (206,'William','Gietz','william.gietz@sqltutorial.org','515.123.8181','1994-06-07',1,83000.00,205,11);
-
+INSERT INTO `sql_tutorial.employees`(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES 
+ (100,'Steven','King','steven.king@sqltutorial.org','515.123.4567','1987-06-17',4,240000.00,NULL,9);
+,(101,'Neena','Kochhar','neena.kochhar@sqltutorial.org','515.123.4568','1989-09-21',5,170000.00,100,9)
+,(102,'Lex','De Haan','lex.de haan@sqltutorial.org','515.123.4569','1993-01-13',5,170000.00,100,9)
+,(103,'Alexander','Hunold','alexander.hunold@sqltutorial.org','590.423.4567','1990-01-03',9,90000.00,102,6)
+,(104,'Bruce','Ernst','bruce.ernst@sqltutorial.org','590.423.4568','1991-05-21',9,60000.00,103,6)
+,(105,'David','Austin','david.austin@sqltutorial.org','590.423.4569','1997-06-25',9,48000.00,103,6)
+,(106,'Valli','Pataballa','valli.pataballa@sqltutorial.org','590.423.4560','1998-02-05',9,48000.00,103,6)
+,(107,'Diana','Lorentz','diana.lorentz@sqltutorial.org','590.423.5567','1999-02-07',9,42000.00,103,6)
+,(108,'Nancy','Greenberg','nancy.greenberg@sqltutorial.org','515.124.4569','1994-08-17',7,120000.00,101,10)
+,(109,'Daniel','Faviet','daniel.faviet@sqltutorial.org','515.124.4169','1994-08-16',6,90000.00,108,10)
+,(110,'John','Chen','john.chen@sqltutorial.org','515.124.4269','1997-09-28',6,82000.00,108,10)
+,(111,'Ismael','Sciarra','ismael.sciarra@sqltutorial.org','515.124.4369','1997-09-30',6,77000.00,108,10)
+,(112,'Jose Manuel','Urman','jose manuel.urman@sqltutorial.org','515.124.4469','1998-03-07',6,78000.00,108,10)
+,(113,'Luis','Popp','luis.popp@sqltutorial.org','515.124.4567','1999-12-07',6,69000.00,108,10)
+,(114,'Den','Raphaely','den.raphaely@sqltutorial.org','515.127.4561','1994-12-07',14,110000.00,100,3)
+,(115,'Alexander','Khoo','alexander.khoo@sqltutorial.org','515.127.4562','1995-05-18',13,31000.00,114,3)
+,(116,'Shelli','Baida','shelli.baida@sqltutorial.org','515.127.4563','1997-12-24',13,29000.00,114,3)
+,(117,'Sigal','Tobias','sigal.tobias@sqltutorial.org','515.127.4564','1997-07-24',13,28000.00,114,3)
+,(118,'Guy','Himuro','guy.himuro@sqltutorial.org','515.127.4565','1998-11-15',13,26000.00,114,3)
+,(119,'Karen','Colmenares','karen.colmenares@sqltutorial.org','515.127.4566','1999-08-10',13,25000.00,114,3)
+,(120,'Matthew','Weiss','matthew.weiss@sqltutorial.org','650.123.1234','1996-07-18',19,80000.00,100,5)
+,(121,'Adam','Fripp','adam.fripp@sqltutorial.org','650.123.2234','1997-04-10',19,82000.00,100,5)
+,(122,'Payam','Kaufling','payam.kaufling@sqltutorial.org','650.123.3234','1995-05-01',19,79000.00,100,5)
+,(123,'Shanta','Vollman','shanta.vollman@sqltutorial.org','650.123.4234','1997-10-10',19,65000.00,100,5)
+,(126,'Irene','Mikkilineni','irene.mikkilineni@sqltutorial.org','650.124.1224','1998-09-28',18,27000.00,120,5)
+,(145,'John','Russell','john.russell@sqltutorial.org',NULL,'1996-10-01',15,140000.00,100,8)
+,(146,'Karen','Partners','karen.partners@sqltutorial.org',NULL,'1997-01-05',15,135000.00,100,8)
+,(176,'Jonathon','Taylor','jonathon.taylor@sqltutorial.org',NULL,'1998-03-24',16,86000.00,100,8)
+,(177,'Jack','Livingston','jack.livingston@sqltutorial.org',NULL,'1998-04-23',16,84000.00,100,8)
+,(178,'Kimberely','Grant','kimberely.grant@sqltutorial.org',NULL,'1999-05-24',16,70000.00,100,8)
+,(179,'Charles','Johnson','charles.johnson@sqltutorial.org',NULL,'2000-01-04',16,62000.00,100,8)
+,(192,'Sarah','Bell','sarah.bell@sqltutorial.org','650.501.1876','1996-02-04',17,40000.00,123,5)
+,(193,'Britney','Everett','britney.everett@sqltutorial.org','650.501.2876','1997-03-03',17,39000.00,123,5)
+,(200,'Jennifer','Whalen','jennifer.whalen@sqltutorial.org','515.123.4444','1987-09-17',3,44000.00,101,1)
+,(201,'Michael','Hartstein','michael.hartstein@sqltutorial.org','515.123.5555','1996-02-17',10,130000.00,100,2)
+,(202,'Pat','Fay','pat.fay@sqltutorial.org','603.123.6666','1997-08-17',11,60000.00,201,2)
+,(203,'Susan','Mavris','susan.mavris@sqltutorial.org','515.123.7777','1994-06-07',8,65000.00,101,4)
+,(204,'Hermann','Baer','hermann.baer@sqltutorial.org','515.123.8888','1994-06-07',12,100000.00,101,7)
+,(205,'Shelley','Higgins','shelley.higgins@sqltutorial.org','515.123.8080','1994-06-07',2,120000.00,101,11)
+,(206,'William','Gietz','william.gietz@sqltutorial.org','515.123.8181','1994-06-07',1,83000.00,205,11)
+;
 
 CREATE TABLE `sql_tutorial.dependents` (
 	dependent_id INTEGER,
